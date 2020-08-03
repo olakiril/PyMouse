b522d2b0d261a3be43d7b4fc9e20c76edbf2002a
eman <olakiril@hotmail.com>
2020-08-03 12:53:02 +0300

    database tables2

--- NewDatabaseGroup.py
+++ NewDatabaseGroup.py
@@ -1,17 +1,16 @@
 import datajoint as dj
 
-ExpSchema = dj.schema('test_experiment')
-BehSchema = dj.schema('test_behavior')
-StimSchema = dj.schema('test_stimuli')
+#ExpSchema = dj.schema('test_experiment')
+#BehSchema = dj.schema('test_behavior')
+#StimSchema = dj.schema('test_stimuli')
+
+experiment = create_virtual_module('experiment.py', 'test_experiment', create_tables=True)
+behavior = create_virtual_module('behavior.py', 'test_behavior', create_tables=True)
+stimulus = create_virtual_module('stimulus.py', 'test_stimuli', create_tables=True)
+
 Mice = dj.create_virtual_module('mice.py', 'lab_mice')
 
-
-def erd():
-    """for convenience"""
-    dj.ERD(schema).draw()
-
-
-@ExpSchema
+@experiment.schema
 class Protocol(dj.Lookup):
     definition = """
     # Behavioral experiment parameters
@@ -22,40 +21,28 @@ class Protocol(dj.Lookup):
     timestamp            : timestamp    
     """
 
-@ExpSchema
-class SetupControl(dj.Lookup):
+@experiment.schema
+class Control(dj.Lookup):
     definition = """
     #
     setup                : varchar(256)                 # Setup name
     ---
     ->Protocol
     ip                   : varchar(16)                  # setup IP address
-    status="ready"       : enum('ready','running','stop','sleeping','offtime','exit') 
+    status="exit"        : enum('ready','running','stop','sleeping','exit','offtime','wakeup') 
     animal_id=null       : int                          # animal id
     last_ping            : timestamp                    
     notes=null           : varchar(256)                 
-    current_session=null : int                          
-    last_trial=null      : int                          
+    current_session=null : smallint                          
+    last_trial=null      : smallint                          
     total_liquid=null    : float     
     state=null           : varchar(256)  
+    difficulty=null      : smallint                     
+    start_time=null      : time                         
+    stop_time=null       : time 
     """
 
-@ExpSchema
-class Session(dj.Manual):
-    definition = """
-    # Behavior session infod
-    animal_id            : int                          # animal id
-    session              : smallint                     # session number
-    ---
-    setup=null           : varchar(256)                 # computer id
-    notes=null           : varchar(2048)                # session notes
-    session_params=null  : mediumblob                   
-    protocol=null        : varchar(256)                 # protocol file
-    experiment_type=null : varchar(256)            
-    session_tmst         : timestamp                    # session timestamp     
-    """
-
-@ExpSchema
+@experiment.schema
 class Condition(dj.Manual):
     definition = """
     # unique stimulus conditions
@@ -64,7 +51,30 @@ class Condition(dj.Manual):
     cond_tuple=null       : blob      
     """
 
-@ExpSchema
+@experiment.schema
+class Session(dj.Manual):
+    definition = """
+    # Behavior session infod
+    -> mice.Mice
+    session              : smallint                     # session number
+    ---
+    setup=null           : varchar(256)                 # computer id
+    notes=null           : varchar(2048)                # session notes
+    session_tmst         : timestamp                    # session timestamp     
+    """
+    class Params(dj.Part):
+        definition = """
+        # Session parameters
+        -> Session
+        ---
+        -> Protocol              
+        protocol=null        : text                     # protocol text
+        session_params=null  : blob                     # parameters for the session
+        experiment_type=null : varchar(256)             
+        git_version=null     : varchar(7)               # git short hash     
+        """
+
+@experiment.schema
 class Trial(dj.Manual):
     definition = """
     # Trial information
@@ -74,11 +84,12 @@ class Trial(dj.Manual):
     -> Condition
     start_time           : int                          # start time from session start (ms)
     end_time             : int                          # end time from session start (ms)
+    aborted              : boolean                      # aborted flag
     """
 
 
 
-@BehSchema
+@behavior.schema
 class CenterPort(dj.Manual):
     definition = """
     # Center port information
@@ -88,7 +99,7 @@ class CenterPort(dj.Manual):
     in_position          : boolean
     """
 
-@BehSchema
+@behavior.schema
 class Lick(dj.Manual):
     definition = """
     # Lick timestamps
@@ -98,20 +109,46 @@ class Lick(dj.Manual):
     """
 
 
+@stimulus.schema
+class Movie(dj.Lookup):
+    definition = """
+    # movies used for generating clips
+    movie_name                              : char(8)       # short movie title
+    ---
+    -> stim.MovieClass
+    path                                    : varchar(255)
+    original_file                           : varchar(255)
+    file_template                           : varchar(255)  # filename template with full path
+    file_duration                           : float         # (s) duration of each file (must be equal)
+    codec="-c:v libx264 -preset slow -crf 5": varchar(255)
+    movie_description                       : varchar(255)  # full movie title
+    frame_rate=30                           : float         # frames per second
+    frame_width=256                         : int           # pixels
+    frame_height=144                        : int           # pixels
+    """
+    class Clip(dj.Part):
+        definition = """
+        # clips from movies
+        -> stim.Movie
+        clip_number                        : smallint       # clip index
+        ---
+        file_name                          : varchar(255)   # full file name
+        clip                               : longblob       # stored clip
+        parent_file_name=null              : varchar(255)
+        duration=null                      : smallint       # duration in seconds
+        """
 
-@StimSchema
+@stimulus.schema
 class Clip(dj.Manual):
     definition = """
     # movie clip conditions
     mov_hash             : char(24)                     # unique condition hash
     ---
-    movie_name           : char(8)                      # short movie title
-    clip_number          : int                          # clip index
-    movie_duration       : int                          # movie duration
+    -> Clip
+    movie_duration       : smallint                     # movie duration
     skip_time            : smallint                     # start time in clip
     static_frame         : smallint                     # static frame presentation
     """
-
     class Trial(dj.Part):
         definition = """
         # Stimulus onset timestamps
@@ -122,24 +159,22 @@ class Clip(dj.Manual):
         end_time            : int                       # end time from session start (ms)
         """
 
-@StimSchema
+@stimulus.schema
 class Odor(dj.Manual):
     definition = """
     # odor conditions
     odor_hash                : char(24)                 # unique condition hash
     """
-
     class Channel(dj.Part):
         definition = """
         # odor conditions
         -> Odor
-        odorant_id           : int                      # index for odorant identity
+        odorant_id           : smallint                 # index for odorant identity
         ---
-        duration             : int                      # odor duration (ms)
-        delivery_channel     : int                      # delivery idx for channel mapping
-        mix            : int                      # odorant ratio in the mixture(0-100)
+        duration             : smallint                 # odor duration (ms)
+        delivery_channel     : smallint                 # delivery idx for channel mapping
+        mix                  : smallint                 # odorant ratio in the mixture(0-100)
         """
-
     class Trial(dj.Part):
         definition = """
         # Stimulus onset timestamps
@@ -150,17 +185,16 @@ class Odor(dj.Manual):
         end_time            : int                        # end time from session start (ms)
         """
 
-@StimSchema
+@stimulus.schema
 class Reward(dj.Manual):
     definition = """
     # reward probe conditions
     rew_hash               : char(24)                     # unique reward hash
     ---
-    port=0                 : int                          # delivery port
+    port=0                 : smallint                     # delivery port
     reward_amount=0        : float                        # reward amount
     reward_type='water'    : enum('water','juice','food') # reward type
     """
-
     class Trial(dj.Part):
         definition = """
         # movie clip conditions
@@ -170,15 +204,14 @@ class Reward(dj.Manual):
         time			      : int 	                # time from session start (ms)
         """
 
-@StimSchema
+@stimulus.schema
 class LiquidCalibration(dj.Manual):
     definition = """
     # Liquid delivery calibration sessions for each probe
     setup                        : varchar(256)         # Setup name
-    port                         : int                   # port number
+    port                         : smallint             # port number
     date                         : date                 # session date (only one per day is allowed)
     """
-
     class PulseWeight(dj.Part):
         definition = """
         # Data for volume per pulse duty cycle estimation
@@ -189,3 +222,4 @@ class LiquidCalibration(dj.Manual):
         weight                   : float                # weight of total liquid released in gr
         timestamp                : timestamp            # timestamp
         """
+
